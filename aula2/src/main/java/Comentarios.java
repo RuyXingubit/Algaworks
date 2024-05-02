3a1976d0704b39b3ebbf2ddc862cc84152cbb68e
Ruy Françca <ruy@xingubit.com.br>
2024-05-02 15:36:52 -0300

    2.4. Escrevendo comentários no código

---
+++ aula2/src/main/java/Comentarios.java
@@ -0,0 +1,14 @@
+public class Comentarios {
+    public static void main(String[] args) {
+
+        /*
+        aula sobre comentario de codigo, este por exemplo é um comentario em bloco,
+        podendo ser usado para comentarios muito grande(algo não recomendado)
+        ou para remover partes do codigo quando se esta buscando algum bug ou outras
+        formas de fazer algo
+         */
+        // ja este é um comentario de linha,
+        // como tal, é necessario duas barras sempre no inicio da linha
+        System.out.print("Aula sobre comentario de codigo.");
+    }
+}
