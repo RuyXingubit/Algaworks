730117c872bbe92e016c891708872d42b39c7a55
Ruy Françca <ruy@xingubit.com.br>
2024-05-02 15:44:28 -0300

    Corrigido Ortografia,

--- aula2/src/main/java/Comentarios.java
+++ aula2/src/main/java/Comentarios.java
@@ -2,13 +2,13 @@ public class Comentarios {
     public static void main(String[] args) {
 
         /*
-        aula sobre comentario de codigo, este por exemplo é um comentario em bloco,
-        podendo ser usado para comentarios muito grande(algo não recomendado)
-        ou para remover partes do codigo quando se esta buscando algum bug ou outras
+        aula sobre comentário de código, este por exemplo é um comentário em bloco,
+        podendo ser usado para comentários muito grande(algo não recomendado)
+        ou para remover partes do código quando se esta buscando algum bug ou outras
         formas de fazer algo
          */
-        // ja este é um comentario de linha,
-        // como tal, é necessario duas barras sempre no inicio da linha
-        System.out.print("Aula sobre comentario de codigo.");
+        // ja este é um comentário de linha,
+        // como tal, é necessário duas barras sempre no inicio da linha
+        System.out.print("Aula sobre comentário de código.");
     }
 }
